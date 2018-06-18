bc5eb6f20e25ce3b6d82dd2108ba3b2dc11430a7
Pawel Kowalski <pawel.kowalski@iterativ.ch>
2018-06-18 18:44:25 +0200

    split pages & components

--- client/src/components/ContainerTeaser.vue
+++ client/src/components/ContainerTeaser.vue
@@ -1,55 +1,33 @@
 <template>
   <div>
+    <v-card-title primary-title>
+      <div>
+        <h2 class="headline mb-1">{{container.title}}</h2>
 
-    <v-card v-for="container in containers" v-bind:key="container.id" class="mb-1">
-      <v-card-title primary-title>
-        <div>
-          <h2 class="headline mb-1">{{container.title}}</h2>
-          <div class="mt-2" v-for="(item, index) in container.units.edges" v-bind:key="index">
-            {{item.node.type}}: {{item.node.title}}
-            {{item.node.teaser}}
-          </div>
-        </div>
-      </v-card-title>
-      <!-- <v-card-actions>
-        <v-btn flat color="orange">Share</v-btn>
-        <v-btn flat color="orange">Explore</v-btn>
-      </v-card-actions> -->
-    </v-card>
+        <!-- <router-link :to="{name: 'Category', params: {id: category['.key']}}">
+          {{ category.name }}
+        </router-link> -->
 
+        <!-- <div class="mt-2" v-for="(item, index) in container.units.edges" v-bind:key="index">
+          {{item.node.type}}: {{item.node.title}}
+          {{item.node.teaser}}
+        </div> -->
+      </div>
+    </v-card-title>
   </div>
 </template>
 
 <script>
-  import store from '@/store/core'
-  import CONTAINERS_QUERY from '@/graphql/gql/containers'
+  // import store from '@/store/core'
+  // import CONTAINERS_QUERY from '@/graphql/gql/containers'
 
   export default {
-    data() {
-      return {
-        store: store,
-        initialQuery: CONTAINERS_QUERY,
-        containers: []
-      }
-    },
-    apollo: {
-      containers: {
-        query: CONTAINERS_QUERY,
-        manual: true,
-        result({data: {containers: {edges}}, loading, networkStatus}) {
-          if (!loading) {
-            // debugger
-            let _containers = []
-            // let self = this
-            edges.forEach(function (container) {
-              console.log(container)
-              // let node = self.$lodash.clone(item.node)
-              // node.content = JSON.parse(node.content)
-              _containers.push(container.node)
-            })
-            this.containers = _containers
-          }
-        }
+    name: 'container-teaser',
+
+    props: {
+      container: {
+        required: true,
+        type: Object
       }
     }
   }
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,16 +1,23 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Categories from '@/pages/PageCategories'
+import Containers from '@/pages/PageContainers'
+import Container from '@/pages/PageContainer'
+import About from '@/pages/PageAbout'
+import NotFound from '@/pages/PageNotFound'
 
 const routerOptions = [
-  {path: '/', component: 'Categories', name: 'categories'},
-  {path: '/containers', component: 'Containers', name: 'containers'},
-  {path: '/about', component: 'About', name: 'about'},
-  {path: '*', component: 'NotFound'}
+  { path: '/', name: 'categories', component: Categories },
+  { path: '/containers', name: 'containers', component: Containers },
+  { path: '/container', name: 'container', component: Container },
+  { path: '/about', name: 'about', component: About },
+  { path: '*', component: NotFound }
 ]
 const routes = routerOptions.map(route => {
   return {
-    ...route,
-    component: () => import(`@/components/${route.component}.vue`)
+    ...route
+    // TODO: nice..
+    // component: () => import(`@/components/${route.component}.vue`)
   }
 })
 Vue.use(Router)
