a0d6f3a6b515d3488a2c263dec5a24ba0b216520
Pawel Kowalski <pawel.kowalski@iterativ.ch>
2018-06-18 16:01:09 +0200

    implement categories and containers vue matching the backend

--- client/src/components/ContainerTeaser.vue
+++ client/src/components/ContainerTeaser.vue
@@ -5,18 +5,16 @@
       <v-card-title primary-title>
         <div>
           <h2 class="headline mb-1">{{container.title}}</h2>
-          <div class="mt-2" v-for="(item, index) in container.content" v-bind:key="index">
-            <div v-if="item.type=='text'" v-html="item.value.text"></div>
-            <div v-else>
-              {{item.type}}: {{item.value}}
-            </div>
+          <div class="mt-2" v-for="(item, index) in container.units.edges" v-bind:key="index">
+            {{item.node.type}}: {{item.node.title}}
+            {{item.node.teaser}}
           </div>
         </div>
       </v-card-title>
-      <v-card-actions>
+      <!-- <v-card-actions>
         <v-btn flat color="orange">Share</v-btn>
         <v-btn flat color="orange">Explore</v-btn>
-      </v-card-actions>
+      </v-card-actions> -->
     </v-card>
 
   </div>
@@ -24,7 +22,7 @@
 
 <script>
   import store from '@/store/core'
-  import CONTAINERS_QUERY from '@/graphql/containers'
+  import CONTAINERS_QUERY from '@/graphql/gql/containers'
 
   export default {
     data() {
@@ -40,14 +38,16 @@
         manual: true,
         result({data: {containers: {edges}}, loading, networkStatus}) {
           if (!loading) {
-            let _items = []
-            let self = this
-            edges.forEach(function (item) {
-              let node = self.$lodash.clone(item.node)
-              node.content = JSON.parse(node.content)
-              _items.push(node)
+            // debugger
+            let _containers = []
+            // let self = this
+            edges.forEach(function (container) {
+              console.log(container)
+              // let node = self.$lodash.clone(item.node)
+              // node.content = JSON.parse(node.content)
+              _containers.push(container.node)
             })
-            this.containers = _items
+            this.containers = _containers
           }
         }
       }
