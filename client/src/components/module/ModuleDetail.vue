d15d6844180e61021947396e5eb5acfa0655961b
Pawel Kowalski <pawel.kowalski@iterativ.ch>
2018-07-10 16:00:56 +0200

    refactor to use a ModuleDetailBooked and ModuleDetail

---
+++ client/src/components/module/ModuleDetail.vue
@@ -0,0 +1,107 @@
+<template>
+  <div>
+    <v-container class="mt-3">
+      <span class="grey--text">{{module.category.title}} - {{module.skill}}</span><br>
+      <h2 class="mb-1 mt-1">{{module.title}}</h2>
+      <v-btn class="button item2 hidden-sm-and-down">
+        Container buchen
+      </v-btn>
+
+    </v-container>
+    <div class="hero-bg mb-4">
+      <v-container grid-list-md>
+        <v-layout row wrap>
+          <v-flex xs12 sm6 md6 lg6 xl6>
+            <div v-html="module.description" class="mb-2 mt-1 description"></div>
+
+            <div v-if="module.videoId" class="mt-5 mb-1">
+              <a href="#" @click.stop="showVideoPlayer=true">
+                <img class="video-thumbnail" :src="module.videoThumbnailData.thumbnail_url_with_play_button"/>
+              </a>
+              <div v-html="module.videoDescription"></div>
+              <VideoPlayer :visible="showVideoPlayer" :videoId="module.videoId" @close="showVideoPlayer=false"/>
+            </div>
+
+          </v-flex>
+          <v-flex xs12 sm6 md6 lg6 xl6>
+            <img :src="module.heroImage"/>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </div>
+
+    <v-container grid-list-xl>
+      <v-layout row wrap>
+        <v-flex xs12 sm8 md8 lg8 xl8>
+          <h3>Lernangebote</h3>
+          <Unit v-for="(item, index) in module.units.edges" :key="index" :unit="item.node" class="mb-4"></Unit>
+        </v-flex>
+        <v-flex xs12 sm4 md4 lg4 xl4>
+          <h3>Ressourcen</h3>
+          <div>Book to see resources - lamp</div>
+          <h3>Tools & Templates</h3>
+          <div>Book to see tools - lamp</div>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </div>
+</template>
+
+<script>
+  import Tools from '@/components/Tools'
+  import VideoPlayer from '@/components/VideoPlayer'
+  import Unit from '@/components/Unit'
+
+  export default {
+    name: 'module-detail',
+
+    props: {
+      module: {
+        required: true,
+        type: Object
+      }
+    },
+
+    components: {
+      Tools,
+      VideoPlayer,
+      Unit
+    },
+
+    data() {
+      return {
+        showVideoPlayer: false
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  @import "../../styles/var";
+
+  img {
+    max-width: 100%;
+  }
+
+  h2 {
+    font-size: 44px;
+  }
+
+  h3 {
+    font-size: 24px;
+    margin-bottom: 12px;
+  }
+
+  .video-thumbnail {
+    max-width: 200px;
+  }
+
+  .hero-bg {
+    background-color: $blue;
+  }
+
+  .description {
+    font-size: 18px;
+    font-weight: bold;
+  }
+</style>
--- client/src/pages/PageModule.vue
+++ client/src/pages/PageModule.vue
@@ -1,45 +1,7 @@
 <template>
   <div v-if="module">
-    <v-container class="mt-3">
-      <span v-if="moduleBooked" class="grey--text">BOOKED&nbsp;</span><span class="grey--text">{{module.category.title}} - {{module.skill}}</span><br>
-      <h2 class="mb-1 mt-1">{{module.title}}</h2>
-    </v-container>
-    <div class="hero-bg mb-4">
-      <v-container grid-list-md>
-        <v-layout row wrap>
-          <v-flex xs12 sm6 md6 lg6 xl6>
-            <div v-html="module.description" class="mb-2 mt-1 description"></div>
-
-            <div v-if="module.videoId" class="mt-5 mb-1">
-              <a href="#" @click.stop="showVideoPlayer=true">
-                <img class="video-thumbnail" :src="module.videoThumbnailData.thumbnail_url_with_play_button"/>
-              </a>
-              <div v-html="module.videoDescription"></div>
-              <VideoPlayer :visible="showVideoPlayer" :videoId="module.videoId" @close="showVideoPlayer=false"/>
-            </div>
-
-          </v-flex>
-          <v-flex xs12 sm6 md6 lg6 xl6>
-            <img :src="module.heroImage"/>
-          </v-flex>
-        </v-layout>
-      </v-container>
-    </div>
-
-    <v-container grid-list-xl>
-      <v-layout row wrap>
-        <v-flex xs12 sm8 md8 lg8 xl8>
-          <h3>Lernangebote</h3>
-          <Unit v-for="(item, index) in module.units.edges" :key="index" :unit="item.node"  class="mb-4"></Unit>
-        </v-flex>
-        <v-flex xs12 sm4 md4 lg4 xl4>
-          <h3>Ressourcen</h3>
-          <Tools :tools="module.tools"/>
-          <h3>Tools & Templates</h3>
-          <Tools :tools="module.resources"/>
-        </v-flex>
-      </v-layout>
-    </v-container>
+    <ModuleDetail v-if="moduleBooked" :module="module"></ModuleDetail>
+    <ModuleDetailBooked v-if="!moduleBooked" :module="module"></ModuleDetailBooked>
   </div>
 </template>
 
@@ -47,9 +9,8 @@
   // import MODULE_QUERY from '@/graphql/gql/module.gql'
   import MODULE_QUERY from '@/graphql/gql/moduleAndModuleProgress.gql'
 
-  import Tools from '@/components/Tools'
-  import VideoPlayer from '@/components/VideoPlayer'
-  import Unit from '@/components/Unit'
+  import ModuleDetail from '@/components/module/ModuleDetail'
+  import ModuleDetailBooked from '@/components/module/ModuleDetailBooked'
 
   export default {
     name: 'page-module',
@@ -62,16 +23,14 @@
     },
 
     components: {
-      Tools,
-      VideoPlayer,
-      Unit
+      ModuleDetail,
+      ModuleDetailBooked
     },
 
     data() {
       return {
         module: null,
-        moduleBooked: false,
-        showVideoPlayer: false
+        moduleBooked: false
       }
     },
 
