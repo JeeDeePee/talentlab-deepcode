efde23573d9861161ccfdd5b3c52f3417c2a0f94
maersu <me@maersu.ch>
2018-08-07 13:07:23 +0200

    reorder import

--- client/src/main.js
+++ client/src/main.js
@@ -1,6 +1,6 @@
 import 'babel-polyfill'
-import Vue from 'vue'
 import 'vuetify/dist/vuetify.min.css'
+import Vue from 'vue'
 import Vuetify from 'vuetify'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
