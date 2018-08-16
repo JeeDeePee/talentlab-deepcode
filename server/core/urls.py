4ba4cb2e59ae4c6cbd975b2e4db2c69d1f547fd4
Pawel Kowalski <pawel.kowalski@iterativ.ch>
2018-08-17 00:14:09 +0200

    wagtail upgrade implies some changes in urls

--- server/core/urls.py
+++ server/core/urls.py
@@ -1,23 +1,17 @@
-from django.conf.urls import url, include
-from django.urls import re_path
-from django.contrib import admin
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-
-from wagtail.core import urls as wagtail_urls
-from wagtail.admin import urls as wagtailadmin_urls
-from wagtail.documents import urls as wagtaildocs_urls
-from wagtail.search import urls as wagtailsearch_urls
-from core import views
 from django.conf import settings
+from django.conf.urls import url, include
 from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import re_path
+from wagtail.admin import urls as wagtailadmin_urls
+from wagtail.core import urls as wagtail_urls
+
+from core import views
 
 urlpatterns = [
     url(r'^guru/', admin.site.urls),
     url(r'^accounts/', include('django.contrib.auth.urls')),
     url(r'^cms/', include(wagtailadmin_urls)),
-    url(r'^search/', include(wagtailsearch_urls)),
-    url(r'^documents/', include(wagtaildocs_urls)),
-
     url(r'^api/', include('api.urls', namespace="api")),
 ]
 
