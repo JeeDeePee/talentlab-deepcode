5b4aadf2c8a409e5eb71848eb1c847452b083148
Pawel Kowalski <pawel.kowalski@iterativ.ch>
2018-08-22 11:18:09 +0200

    move user filter class into a parametrized class

--- server/focus/schema/schema.py
+++ server/focus/schema/schema.py
@@ -4,6 +4,7 @@ from graphene import relay
 from graphene_django import DjangoObjectType
 from graphene_django.filter import DjangoFilterConnectionField
 
+from core.graphql_filter import graphql_user_filter
 from core.middleware import get_current_user
 from focus.models import Competence, CompetenceEntry, Focus
 
@@ -32,18 +33,21 @@ class FocusNode(DjangoObjectType):
         return self.id
 
 
-class UserFocusFilter(django_filters.FilterSet):
-    class Meta:
-        model = Focus
-        fields = ['active']
+# class UserFocusFilter(django_filters.FilterSet):
+#     class Meta:
+#         model = Focus
+#         fields = ['active']
+#
+#     @property
+#     def qs(self):
+#         user = get_current_user()
+#         if not user:
+#             return Focus.objects.none()
+#         return super(UserFocusFilter, self).qs.filter(user=user)
 
-    @property
-    def qs(self):
-        user = get_current_user()
-        if not user:
-            return Focus.objects.none()
-        return super(UserFocusFilter, self).qs.filter(user=user)
+
+filterset_class = graphql_user_filter(Focus, ['active'])
 
 
 class UserFocusQuery(object):
-    user_focus = DjangoFilterConnectionField(FocusNode, filterset_class=UserFocusFilter)
+    user_focus = DjangoFilterConnectionField(FocusNode, filterset_class=filterset_class)
