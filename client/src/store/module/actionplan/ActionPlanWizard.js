b03f7cd46df5f3615174a7da5abb80870f9c7491
Pawel Kowalski <pawel.kowalski@orbit7.ch>
2018-08-28 17:29:50 +0200

    move generator into a separate file

--- client/src/store/module/actionplan/ActionPlanWizard.js
+++ client/src/store/module/actionplan/ActionPlanWizard.js
@@ -1,27 +1,3 @@
-
-function wizardGenerator(wizardName, defaultWizardStep) {
-  let state = {}
-  state[`${wizardName}WizardState`] = defaultWizardStep
-
-  let mutations = {}
-  mutations[`set${wizardName}WizardState`] = function(state, wizardState) {
-    state[`${wizardName}WizardState`] = wizardState
-  }
-
-  let actions = {}
-  actions[`new${wizardName}WizardState`] = function({state, commit}, newState) {
-    commit(`set${wizardName}WizardState`, newState)
-  }
-
-  let getters = {}
-  getters[`get${wizardName}WizardState`] = state => state[`${wizardName}WizardState`]
-
-  return {
-    state: state,
-    mutations: mutations,
-    actions: actions,
-    getters: getters
-  }
-}
+import wizardGenerator from '@/store/wizardGenerator'
 
 export default wizardGenerator('ActionPlan', 'StartActionPlan')
