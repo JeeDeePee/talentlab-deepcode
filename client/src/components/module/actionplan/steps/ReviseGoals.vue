75af6deaf1fba6df01909d328c743e89ad515d84
Pawel Kowalski <pawel.kowalski@orbit7.ch>
2018-08-29 15:57:07 +0200

    style the button like a pro

--- client/src/components/module/actionplan/steps/ReviseGoals.vue
+++ client/src/components/module/actionplan/steps/ReviseGoals.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="text-xs-center">
     <h1>Rekapituliere Deine Ziele</h1>
-    <p>Dein Ziel zum Container «Digital Communication & Virtual Collaboration». Passe bei Bedarf noch einmal an.</p>
+    <p class="mt-3">Dein Ziel zum Container «Digital Communication & Virtual Collaboration». Passe bei Bedarf noch einmal an.</p>
 
     <div class="module-user-goal pa-3 mt-5 mb-5">{{moduleUserGoal.text}}</div>
 
-    <v-btn @click="$emit('back', 'StartActionPlan')">Zurück</v-btn>
+    <v-btn class="btn-secondary" @click="$emit('back', 'StartActionPlan')">Zurück</v-btn>
     <v-btn @click="$emit('proceed', 'Learnings')">Weiter</v-btn>
   </div>
 </template>
