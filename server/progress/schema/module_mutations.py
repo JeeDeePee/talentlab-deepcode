23d53732bee8fda639bd9d2083c71b3e5b1846f8
Pawel Kowalski <pawel.kowalski@orbit7.ch>
2018-09-03 15:34:03 +0200

    implement define action plan endpoint

--- server/progress/schema/module_mutations.py
+++ server/progress/schema/module_mutations.py
@@ -1,8 +1,10 @@
 import graphene
+from django.shortcuts import get_object_or_404
 
 from core.middleware import get_current_user
 from modules.models import Module
 from progress.models import UserModuleProgress
+from progress.schema.module import UserModuleProgressNode
 
 
 class StartModuleProgress(graphene.Mutation):
@@ -48,6 +50,35 @@ class DeleteModuleProgress(graphene.Mutation):
             return DeleteModuleProgress(deleted=False, module_slug=module_slug, failed=True, exception=ex)
 
 
+class DefineActionPlan(graphene.relay.ClientIDMutation):
+    class Input:
+        module_slug = graphene.String(required=True)
+        # text params
+        impact_text = graphene.String()
+        measurement_text = graphene.String()
+        measures_text = graphene.String()
+        time_frame_text = graphene.String()
+        resources_skills_text = graphene.String()
+        commitment_support_text = graphene.String()
+
+    user_module_progress = graphene.Field(UserModuleProgressNode)
+
+    @classmethod
+    def mutate_and_get_payload(cls, *args, **kwargs):
+        user_module_progress = get_object_or_404(
+            UserModuleProgress,
+            user=get_current_user(),
+            module__slug=kwargs.pop('module_slug')
+        )
+
+        for k, v in kwargs.items():
+            setattr(user_module_progress, k, v)
+
+        user_module_progress.save()
+        return cls(user_module_progress=user_module_progress)
+
+
 class ModuleProgressMutations(object):
     start_module_progress = StartModuleProgress.Field()
     delete_module_progress = DeleteModuleProgress.Field()
+    define_action_plan = DefineActionPlan.Field()
