11b31089dabfb32f5cf6bd99d1c327bc477589e5
Pawel Kowalski <pawel.kowalski@orbit7.ch>
2018-09-04 10:16:31 +0200

    get rid of unnecessary file header

--- server/user/admin.py
+++ server/user/admin.py
@@ -1,12 +1,3 @@
-# -*- coding: utf-8 -*-
-#
-# orbit7 gmbh
-# http://orbit7.ch/
-#
-# Copyright (c) 2018 orbit7 gmbh. All rights reserved.
-#
-# Created on 26/06/18
-# @author: Pawel Kowalski <pawel.kowalski@orbit7.ch>
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User
