7bf7dd43179d413c2b4295f7e13d7663f9d9671b
maersu <me@maersu.ch>
2018-10-08 08:07:09 +0200

    Fix Texts

--- client/src/components/coaching/steps/Coaches.vue
+++ client/src/components/coaching/steps/Coaches.vue
@@ -65,7 +65,7 @@
 
     <div class="mt-5 text-xs-center">
       <v-btn class="btn-secondary mr-2" @click="$emit('back', 'Topics')">Zurück</v-btn>
-      <v-btn @click="save" class="ml-2">Weiter</v-btn>
+      <v-btn @click="save" class="ml-2">Coach auswählen</v-btn>
     </div>
   </div>
 </template>
--- client/src/components/coaching/steps/Organize.vue
+++ client/src/components/coaching/steps/Organize.vue
@@ -41,7 +41,7 @@
 
     <div class="mt-5 text-xs-center">
       <v-btn class="btn-secondary mr-2" @click="$emit('back', 'Coaches')">Zurück</v-btn>
-      <v-btn @click="save" class="ml-2">Weiter</v-btn>
+      <v-btn @click="save" class="ml-2">Coach buchen</v-btn>
     </div>
   </div>
 </template>
