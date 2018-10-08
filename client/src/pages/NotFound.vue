15c7ed84c5afb7b25133241948faf3381d9f6e95
maersu <me@maersu.ch>
2018-10-08 06:47:18 +0200

    Not found page

---
+++ client/src/pages/NotFound.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <v-container>
+      <v-layout
+        column
+        align-center
+        justify-center
+      >
+        <h1 class="mb-3">Seite nicht gefunden</h1>
+
+        <v-btn
+          class="mt-5"
+          large
+          :to="{ name: 'start'}"
+          router
+          exact
+        >
+          Start
+        </v-btn>
+
+      </v-layout>
+    </v-container>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
