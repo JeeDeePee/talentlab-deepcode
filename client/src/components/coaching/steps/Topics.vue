6e20ca645a1d3498d4a5679d7b1ff9e4f50e225f
Pawel Kowalski <pawel.kowalski@orbit7.ch>
2018-11-20 11:03:43 +0100

    fix commas

--- client/src/components/coaching/steps/Topics.vue
+++ client/src/components/coaching/steps/Topics.vue
@@ -42,12 +42,12 @@
           {title: 'Standortbestimmung & Neuorientierung', id: 1},
           {title: 'Domain-Life-Balance', id: 2},
           {title: 'Frauen in Führungspositionen', id: 3},
-          {title: 'Job Sharing', id: 4}
+          {title: 'Job Sharing', id: 4},
           {title: 'Persönliche oder berufliche Veränderungen und Krisen', id: 5},
           {title: 'Übernahme einer neuen Rolle', id: 6},
           {title: 'Übernahme einer Führungsfunktion', id: 7},
-          {title: 'Führung von Führungskräften', id: 8}
-          {title: 'Bewältigung anspruchsvoller Führungssituationen', id: 9}
+          {title: 'Führung von Führungskräften', id: 8},
+          {title: 'Bewältigung anspruchsvoller Führungssituationen', id: 9},
           {title: 'Vorbereitung auf individuelle Herausforderungen', id: 10}
         ]
       }
