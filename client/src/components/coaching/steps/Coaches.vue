4a7a74aed546948114bf0ed57b57525e54de30ae
Clea <clea.bauch@cbpoint.ch>
2018-11-20 13:43:19 +0100

    Text Lis

--- client/src/components/coaching/steps/Coaches.vue
+++ client/src/components/coaching/steps/Coaches.vue
@@ -155,8 +155,8 @@
             'description': '<p>Lis Spühler Schaffroth:</p>"Gewinnen Sie an Souveränität!"</p>' +
             ' Lis Spühler Schaffroth ist Psychologin mit langjähriger Führungs-, Beratungs- und Coachingerfahrung.' +
             ' Sie hat Weiterbildungen unter anderem in Konfliktmanagement, Mediation und strategischem Management absolviert.' +
-            ' Direkte Anwendbarkeit im Berufsalltag und die Stärkung des Selbst stehen dabei im Zentrum. Gemeinsam werden spezifische Lösungsansätze' +
-            ' zur Erweiterung der persönlichen Handlungsoptionen entwickelt.</p>' +
+            ' Gemeinsam werden spezifische Lösungsansätze zur Erweiterung der persönlichen Handlungsoptionen entwickelt.' +
+            ' Direkte Anwendbarkeit im Berufsalltag und die Stärkung des Selbst stehen dabei im Zentrum.</p>' +
             ' Lis Spühler Schaffroth arbeitet selbständig im Bereich Personal- und Organisationsentwicklung sowie psychologische Beratung.</p>' +
             ' Lis Spühler Schaffroth spricht Deutsch und Französisch.'
           },
