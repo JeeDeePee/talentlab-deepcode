1db61cc9ebbbdad5fb6d29c636aaa69be4f12f0a
maersu <me@maersu.ch>
2019-01-10 09:09:10 +0100

    tmp fix

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.6
+FROM python:3.7
 
 LABEL maintainer="hello@horbit7.ch"
 LABEL description="talentlab web page"
--- client/src/main.js
+++ client/src/main.js
@@ -40,10 +40,10 @@ function getRidOfEdges(collection) {
   }
 }
 
-var openChatEvent = new Event('openChat')
+// var openChatEvent = new Event('openChat')
 
 function openChat() {
-  window.dispatchEvent(openChatEvent)
+  // window.dispatchEvent(openChatEvent)
 }
 
 Object.defineProperty(Vue.prototype, '$getRidOfEdges', {value: getRidOfEdges})
